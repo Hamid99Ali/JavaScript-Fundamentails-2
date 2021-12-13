d7e612e13437c864e49724452a9e681cc45ea57d
Hamid99Ali <mail2hamidali99@gmail.com>
2021-12-13 23:34:04 +0500

    FunctionDeclarationsAndFunctionExpresion.js

---
+++ FunctionDeclarationAndFunctionExpresion.js
@@ -0,0 +1,12 @@
+// Function Declaration:
+function friut(apple) {
+    console.log(`This is ${apple} Apples`);
+    return 20 - apple;
+}
+const calu = friut(5)
+
+const calu2 = function (age) {
+    return 2025 - age;
+}
+const calus = calu2(1999);
+console.log(calu, calus);
